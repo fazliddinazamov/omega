eaf885d54bfc0cd871cd1a4f80627817cd2f05bc
fazliddinazamov <80683738+fazliddinazamov@users.noreply.github.com>
2021-04-14 22:23:09 +0500

    kkjl,;

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+var dollar=9433.34   //1-dollar=9433.34 so'm
+var yevro=10354.03 //1yevro=10354.03so'm
+var umumiy_pul=750*9433.34+10354.03*120
+var alisherni_puli=prompt("Alisherning umumiy puli=")
+if(alisherni_puli>=umumiy_pul){
+    console.log("Oq yo'l Alisher!")
+}
+else{
+    console.log("Alisher ozgina sabr qilishga togri keladi")
+}
